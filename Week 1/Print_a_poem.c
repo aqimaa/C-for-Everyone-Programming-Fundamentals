76d628e6e4ea45844e9da5c09aa5523e6a6ce12c
hita <aqimaaaa@gmail.com>
2025-03-04 23:58:35 +0700

    Create Print_a_poem.c

---
+++ Week 1/Print_a_poem.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("Drifting alone in the silent night, \nStars whisper tales in silver light. \nA comet streaks, a fleeting spark, \nLost in the vast, eternal dark.\n");
+    return 0;
+}
